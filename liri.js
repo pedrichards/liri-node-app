a7abfa85cc4369da3ec646e1371d350adcb324fa
Schisandra <pedrichards@gmail.com>
2019-09-08 18:59:51 -0400

    continued updating

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+var axios = require("axios");
+var fs = require("fs");
 //require moment
 src = "https://cdn.jsdelivr.net/momentjs/2.12.0/moment.min.js"
 
 var keys = require("./keys.js");
+
+
 
 // Grab search command line argument
 var search = process.argv[2];
@@ -37,69 +41,166 @@ if (search === "do-what-it-says") {
 
 }
 // var spotify = new Spotify(keys.spotify);
-findShow = function (term) {
-    var URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + term + "&countryCode=US&apikey=gCvRPaFA7rJ17s1Du0MBzlUE3vOU0T5Y";
+function findSong(word) {
+
+
+    // var URL = "https://api.spotify.com/v1/search?query=" + word + "&offset=0&limit=20&type=track";
 
     // Add code to search the TVMaze API for the given actor
     // The API will return an array containing multiple actors, just grab the first result
     // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
     // Print this information to the console
-    axios.get(URL).then(function (response) {
-        // Place the response.data into a variable, jsonData.
-        var jsonData = response.data[0].person;
+    // axios.get(URL, spotify).then(function (response) {
+    var Spotify = require('node-spotify-api');
 
-        // showData ends up being the string containing the show data we will print to the console
-        var showData = [
-            //             Name of the venue
-            // Venue location
-            // Date of the Event (use moment to format this as "MM/DD/YYYY")
-            "Venue: " + jsonData.name,
-            "Location: " + jsonData.gender,
-            "Event Date: " + jsonData.birthday,
-        ].join("\n\n");
+    var spotify = new Spotify(keys.spotify);
 
-        // Append showData and the divider to log.txt, print showData to the console
-        fs.appendFile("log.txt", actorData + divider, function (err) {
-            if (err) throw err;
-            console.log(actorData);
+    spotify
+        .search({ type: 'track', query: word })
+        .then(function (response) {
+            console.log(response);
+            var jsonData = response.tracks.items[0];
+            // , {
+            //     params: {
+            //       fields: "something",
+            //       access_token:"8e8e8ee08e0e"  
+            //     }
+            //   })
+
+            // showData ends up being the string containing the show data we will print to the console
+            var songData = [
+                // Artist(s)
+                // The song's name
+                // A preview link of the song from Spotify
+                // The album that the song is from
+                "Artist(s)" + jsonData.artists,
+                "Name: " + jsonData.name,
+                "Preview Link " + jsonData.preview_url,
+                "Abum: " + jsonData.album,
+            ].join("\n\n");
+
+            // Append showData and the divider to log.txt, print showData to the console
+            fs.appendFile("log.txt", actorData + divider, function (err) {
+                if (err) throw err;
+                console.log(songData);
+            })
+        })
+        .catch(function (err) {
+            console.log(err);
         });
-    });
-
-
-
-
 };
 
-findSong = function (term) {
-    var URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + term + "&countryCode=US&apikey=gCvRPaFA7rJ17s1Du0MBzlUE3vOU0T5Y";
+//         // Place the response.data into a variable, jsonData.
+//         var jsonData = response.data;
+//         // , {
+//         //     params: {
+//         //       fields: "something",
+//         //       access_token:"8e8e8ee08e0e"  
+//         //     }
+//         //   })
 
-    // Add code to search the TVMaze API for the given actor
-    // The API will return an array containing multiple actors, just grab the first result
-    // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
-    // Print this information to the console
-    axios.get(URL).then(function (response) {
-        // Place the response.data into a variable, jsonData.
-        var jsonData = response.data[0].person;
+//         // showData ends up being the string containing the show data we will print to the console
+//         var songData = [
+//             // Artist(s)
+//             // The song's name
+//             // A preview link of the song from Spotify
+//             // The album that the song is from
+//             "Artist(s)" + jsonData.artists,
+//             "Name: " + jsonData.name,
+//             "Preview Link " + jsonData.preview_url,
+//             "Abum: " + jsonData.album,
+//         ].join("\n\n");
 
-        // showData ends up being the string containing the show data we will print to the console
-        var showData = [
-            //             Name of the venue
-            // Venue location
-            // Date of the Event (use moment to format this as "MM/DD/YYYY")
-            "Venue: " + jsonData.name,
-            "Location: " + jsonData.gender,
-            "Event Date: " + jsonData.birthday,
-        ].join("\n\n");
-
-        // Append showData and the divider to log.txt, print showData to the console
-        fs.appendFile("log.txt", actorData + divider, function (err) {
-            if (err) throw err;
-            console.log(actorData);
-        });
-    });
-
-
-
-
-};
+//         // Append showData and the divider to log.txt, print showData to the console
+//         fs.appendFile("log.txt", actorData + divider, function (err) {
+//             if (err) throw err;
+//             console.log(songData);
+//         });
+//     });
 // };
+
+function findMovie(word) {
+    var URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + word + "&countryCode=US&apikey=gCvRPaFA7rJ17s1Du0MBzlUE3vOU0T5Y";
+
+    // Add code to search the TVMaze API for the given actor
+    // The API will return an array containing multiple actors, just grab the first result
+    // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
+    // Print this information to the console
+    axios.get(URL).then(function (response) {
+        // Place the response.data into a variable, jsonData.
+        var jsonData = response.data[0].person;
+
+        // showData ends up being the string containing the show data we will print to the console
+        var movieData = [
+            //             Name of the venue
+            // Venue location
+            // Date of the Event (use moment to format this as "MM/DD/YYYY")
+            "Venue: " + jsonData.name,
+            "Location: " + jsonData.gender,
+            "Event Date: " + jsonData.birthday,
+        ].join("\n\n");
+
+        // Append showData and the divider to log.txt, print showData to the console
+        fs.appendFile("log.txt", movieData + divider, function (err) {
+            if (err) throw err;
+            console.log(movieData);
+        });
+    });
+};
+function findShow(word) {
+    var URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + word + "&countryCode=US&apikey=gCvRPaFA7rJ17s1Du0MBzlUE3vOU0T5Y";
+
+    // Add code to search the TVMaze API for the given actor
+    // The API will return an array containing multiple actors, just grab the first result
+    // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
+    // Print this information to the console
+    axios.get(URL).then(function (response) {
+        // Place the response.data into a variable, jsonData.
+        var jsonData = response.data[0].person;
+
+        // showData ends up being the string containing the show data we will print to the console
+        var movieData = [
+            //             Name of the venue
+            // Venue location
+            // Date of the Event (use moment to format this as "MM/DD/YYYY")
+            "Venue: " + jsonData.name,
+            "Location: " + jsonData.gender,
+            "Event Date: " + jsonData.birthday,
+        ].join("\n\n");
+
+        // Append showData and the divider to log.txt, print showData to the console
+        fs.appendFile("log.txt", movieData + divider, function (err) {
+            if (err) throw err;
+            console.log(movieData);
+        });
+    });
+};
+    // };
+    // findShow = function (word) {
+    //     var URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + word + "&countryCode=US&apikey=gCvRPaFA7rJ17s1Du0MBzlUE3vOU0T5Y";
+
+    //     // Add code to search the TVMaze API for the given actor
+    //     // The API will return an array containing multiple actors, just grab the first result
+    //     // Append the actor's name, birthday, gender, country, and URL to the `log.txt` file
+    //     // Print this information to the console
+    //     axios.get(URL).then(function (response) {
+    //         // Place the response.data into a variable, jsonData.
+    //         var jsonData = response.data[0].person;
+
+    //         // showData ends up being the string containing the show data we will print to the console
+    //         var showData = [
+    //             //             Name of the venue
+    //             // Venue location
+    //             // Date of the Event (use moment to format this as "MM/DD/YYYY")
+    //             "Venue: " + jsonData.name,
+    //             "Location: " + jsonData.gender,
+    //             "Event Date: " + jsonData.birthday,
+    //         ].join("\n\n");
+
+    //         // Append showData and the divider to log.txt, print showData to the console
+    //         fs.appendFile("log.txt", showData + divider, function (err) {
+    //             if (err) throw err;
+    //             console.log(showData);
+    //         });
+    //     });
+    // };
